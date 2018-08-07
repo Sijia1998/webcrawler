e5a97ddcec1e38033a5655cd4465619ab374be0f
Sijia1998 <894744332@qq.com>
2018-08-07 14:30:57 +0800

    小爬虫

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ const path = require('path');
 const util = require('util');
 const url = require('url');
 
+const http = require('http');
 
-let getNewsList = (done) => {
-    let news = new Array();
-    request('http://www.cnblogs.com', (err, res, data) => {
-        if (err) {
-            return console.error(err);
-        } else {
-            const $ = cheerio.load(data);
-            let navText = $('.post_nav_block').html()
-            console.log(navText);
-        }
-    })
-}
-getNewsList()
+let server = http.createServer((req, response) => {
+    
+
+    let getNewsList = (done) => {
+        let news = new Array();
+        request('http://www.cnblogs.com', (err, res, data) => {
+            if (err) {
+                return console.error(err);
+            } else {
+                let $ = cheerio.load(data);
+                let navText = $('.post_nav_block').html()
+                console.log(navText);
+                response.end(navText);
+            }
+        })
+    }
+    getNewsList();
+    
+})
+server.listen(8080);
+
+
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-function download(url,callback){
-    http.get(url,function(res){
-        let data = "";
-    })
-}
